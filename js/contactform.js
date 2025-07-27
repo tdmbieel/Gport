6c756216036675c71e4d3515b427b425629f4348
tdmbieel <tdmbieldc@gmail.com>
2025-07-26 22:19:57 -0300

    Add files via upload

---
+++ js/contactform.js
@@ -0,0 +1,6 @@
+function showResponse(responseText,statusText){if(statusText=='success'){jQuery('#contact-container').html('<h3>Message sent</h3>');jQuery('#output').html('<p>'+ jQuery('someText',responseText).html()+'</p>');}else{alert('status: '+ statusText+'\n\nSomething is wrong here');}}
+function showRequest(formData,jqForm,options){var form=jqForm[0];var validRegExp=/^[^@]+@[^@]+.[a-z]{2,}$/i;if(!form.author.value){jQuery('#output').html('<div class="output2">Please fill the Name field!</div>');return false;}else if(!form.email.value){jQuery('#output').html('<div class="output2">Please fill the Email field!</div>');return false;}else if(form.email.value.search(validRegExp)==-1){jQuery('#output').html('<div class="output2">Please provide a valid Email address!</div>');return false;}
+else if(!form.title.value){jQuery('#output').html('<div class="output2">Please fill the Subject field!</div>');return false;}
+else if(!form.message.value){jQuery('#output').html('<div class="output2">Please fill the Message field!</div>');return false;}
+else{jQuery('#output').html('Sending message...!');return true;}}
+jQuery(document).ready(function(){var options={success:showResponse,beforeSubmit:showRequest};jQuery('#my-form').submit(function(){jQuery(this).ajaxSubmit(options);return false;});});
---
+++ js/switcher.js
@@ -0,0 +1,2 @@
+jQuery(document).ready(function(){jQuery('#style-switch').animate({left:-212});jQuery('#t-row-left-ss').animate({left:0});var selector=1;jQuery('#t-row-left-ss').click(function(){if(selector==1){jQuery('#style-switch').animate({left:0});jQuery('#t-row-left-ss').animate({left:212});$(".color-square").click(function(){var id=$(this).attr("data-col");$("#switch_style").attr("href","css/"+ id+".css");});selector=0;}
+else{jQuery('#style-switch').animate({left:-212});jQuery('#t-row-left-ss').animate({left:0});selector=1;}});});
